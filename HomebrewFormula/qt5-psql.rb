a84bb1702d5637b76a9be16d3df10ed663f11866
Sergey Indlin (mbp-pro) <indlin@icloud.com>
2023-06-29 15:40:35 +0300

    qt 5.15.10

--- HomebrewFormula/qt5-psql.rb
+++ HomebrewFormula/qt5-psql.rb
@@ -1,6 +1,6 @@
 # Patches for Qt must be at the very least submitted to Qt's Gerrit codereview
 # rather than their bug-report Jira. The latter is rarely reviewed by Qt.
-class Qt5-psql < Formula
+class Qt5Psql < Formula
   desc "Cross-platform application and UI framework"
   homepage "https://www.qt.io/"
   # NOTE: Use *.diff for GitLab/KDE patches to avoid their checksums changing.
