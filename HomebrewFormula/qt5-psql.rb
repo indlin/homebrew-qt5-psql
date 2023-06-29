997b5d58b9b13ac95d3b569de2e3fbb3fad6a794
Sergey Indlin (mbp-pro) <indlin@icloud.com>
2023-06-29 16:52:24 +0300

    qt 5.15.10

--- HomebrewFormula/qt5-psql.rb
+++ HomebrewFormula/qt5-psql.rb
@@ -15,7 +15,6 @@ class Qt5Psql < Formula
   depends_on "node"       => :build
   depends_on "pkg-config" => :build
   depends_on "python@3.10" => :build
-  depends_on xcode: :build
   depends_on "freetype"
   depends_on "glib"
   depends_on "jpeg-turbo"
@@ -34,48 +33,6 @@ class Qt5Psql < Formula
 
   fails_with gcc: "5"
 
-  resource "qtwebengine" do
-    url "https://code.qt.io/qt/qtwebengine.git",
-        tag:      "v5.15.11-lts",
-        revision: "3d23b379a7c0a87922f9f5d9600fde8c4e58f1fd"
-
-    # Add Python 3 support to qt-webengine-chromium.
-    # Submitted upstream here: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/416534
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/7ae178a617d1e0eceb742557e63721af949bd28a/qt5/qt5-webengine-chromium-python3.patch?full_index=1"
-      sha256 "a93aa8ef83f0cf54f820daf5668574cc24cf818fb9589af2100b363356eb6b49"
-      directory "src/3rdparty"
-    end
-
-    # Add Python 3 support to qt-webengine.
-    # Submitted upstream here: https://codereview.qt-project.org/c/qt/qtwebengine/+/416535
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/a6f16c6daea3b5a1f7bc9f175d1645922c131563/qt5/qt5-webengine-python3.patch?full_index=1"
-      sha256 "398c996cb5b606695ac93645143df39e23fa67e768b09e0da6dbd37342a43f32"
-    end
-
-    # Fix build of qt-webengine-chromium with newer GCC.
-    # Submitted upstream here: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/416598
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/a6f16c6daea3b5a1f7bc9f175d1645922c131563/qt5/qt5-webengine-gcc12.patch?full_index=1"
-      sha256 "cf9be3ffcc3b3cd9450b1ff13535ff7d76284f73173412d097a6ab487463a379"
-      directory "src/3rdparty"
-    end
-
-    # Fix build for Xcode 14
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/405b6b7ca7b95860ee70368076382b171a1c66f4/qt5/qt5-webengine-xcode14.diff"
-      sha256 "142c4fb11dca6c0bbc86ca8f74410447c23be1b1d314758515bfda20afa6f612"
-      directory "src/3rdparty"
-    end
-  end
-
-  # Update catapult to a revision that supports Python 3.
-  resource "catapult" do
-    url "https://chromium.googlesource.com/catapult.git",
-    revision: "5eedfe23148a234211ba477f76fc2ea2e8529189"
-  end
-
   # Fix build with Xcode 14.3.
   patch do
     url "https://invent.kde.org/qt/qt/qtlocation-mapboxgl/-/commit/5a07e1967dcc925d9def47accadae991436b9686.diff"
@@ -84,18 +41,6 @@ class Qt5Psql < Formula
   end
 
   def install
-    rm_r "qtwebengine"
-
-    resource("qtwebengine").stage(buildpath/"qtwebengine")
-
-    rm_r "qtwebengine/src/3rdparty/chromium/third_party/catapult"
-
-    resource("catapult").stage(buildpath/"qtwebengine/src/3rdparty/chromium/third_party/catapult")
-
-    # FIXME: GN requires clang in clangBasePath/bin
-    inreplace "qtwebengine/src/3rdparty/chromium/build/toolchain/mac/BUILD.gn",
-       'rebase_path("$clang_base_path/bin/", root_build_dir)', '""'
-
     args = %W[
       -verbose
       -prefix #{prefix}
@@ -117,34 +62,6 @@ class Qt5Psql < Formula
     if OS.mac?
       args << "-no-rpath"
       args << "-no-assimp" if Hardware::CPU.arm?
-    else
-      args << "-R#{lib}"
-      # https://bugreports.qt.io/browse/QTBUG-71564
-      args << "-no-avx2"
-      args << "-no-avx512"
-      args << "-no-sql-mysql"
-
-      # Use additional system libraries on Linux.
-      # Currently we have to use vendored ffmpeg because the chromium copy adds a symbol not
-      # provided by the brewed version.
-      # See here for an explanation of why upstream ffmpeg does not want to add this:
-      # https://www.mail-archive.com/ffmpeg-devel@ffmpeg.org/msg124998.html
-      # On macOS chromium will always use bundled copies and the webengine_*
-      # arguments are ignored.
-      args += %w[
-        -system-harfbuzz
-        -webengine-alsa
-        -webengine-icu
-        -webengine-kerberos
-        -webengine-opus
-        -webengine-pulseaudio
-        -webengine-webp
-      ]
-
-      # Homebrew-specific workaround to ignore spurious linker warnings on Linux.
-      inreplace "qtwebengine/src/3rdparty/chromium/build/config/compiler/BUILD.gn",
-               "fatal_linker_warnings = true",
-               "fatal_linker_warnings = false"
     end
 
     ENV.prepend_path "PATH", Formula["python@3.10"].libexec/"bin"
@@ -207,10 +124,6 @@ class Qt5Psql < Formula
     # of both Designer and Linguist as that relies on Assistant being in `bin`.)
     libexec.mkpath
     Pathname.glob("#{bin}/*.app") { |app| mv app, libexec }
-
-    # Fix find_package call using QtWebEngine version to find other Qt5 modules.
-    inreplace Dir[lib/"cmake/Qt5WebEngine*/*Config.cmake"],
-              " #{resource("qtwebengine").version} ", " #{version} "
   end
 
   def caveats
@@ -223,38 +136,5 @@ class Qt5Psql < Formula
       pressing "Choose..." and selecting as the Qt installation path:
         #{opt_prefix}
     EOS
-  end
-
-  test do
-    (testpath/"hello.pro").write <<~EOS
-      QT       += core
-      QT       -= gui
-      TARGET = hello
-      CONFIG   += console
-      CONFIG   -= app_bundle
-      TEMPLATE = app
-      SOURCES += main.cpp
-    EOS
-
-    (testpath/"main.cpp").write <<~EOS
-      #include <QCoreApplication>
-      #include <QDebug>
-
-      int main(int argc, char *argv[])
-      {
-        QCoreApplication a(argc, argv);
-        qDebug() << "Hello World!";
-        return 0;
-      }
-    EOS
-
-    # Work around "error: no member named 'signbit' in the global namespace"
-    ENV.delete "CPATH"
-
-    system bin/"qmake", testpath/"hello.pro"
-    system "make"
-    assert_predicate testpath/"hello", :exist?
-    assert_predicate testpath/"main.o", :exist?
-    system "./hello"
   end
 end
